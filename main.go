cb70ef199f77c7fa9fb76152a4dae40e43f352aa
Ethan Denny <ethandenny@protonmail.com>
2024-08-23 19:59:37 -0230

    Working + random

--- main.go
+++ main.go
@@ -6,17 +6,48 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"strconv"
 )
 
-func main() {
-	fmt.Print("Please enter an xkcd comic number: ")
-	var number string
-	fmt.Scan(&number)
+const comicPrefix = "https://xkcd.com/"
+const randomLink = "https://c.xkcd.com/random/comic/"
 
-	content := getHTML("https://xkcd.com/" + number)
-	re := regexp.MustCompile(`https://imgs.xkcd.com/comics/.*\.png`)
-	link := re.FindString(content)
+var comicNameRegex = regexp.MustCompile(`<div id="ctitle">(.*?)</div>`)
+var imageLinkRegex = regexp.MustCompile(`https://imgs.xkcd.com/comics/.*\.(png|jpg)`)
+
+func main() {
+	name, link := getRandomComic()
+	fmt.Println(name)
 	fmt.Println(link)
+}
+
+func getComicName(content *string) string {
+	matches := comicNameRegex.FindStringSubmatch(*content)
+	if len(matches) > 1 {
+		return matches[1]
+	} else {
+		return ""
+	}
+}
+
+func getImageLink(content *string) string {
+	return imageLinkRegex.FindString(*content)
+}
+
+func getComic(number int) (string, string) {
+	content := getHTML(comicPrefix + strconv.Itoa(number))
+	return getComicDetails(&content)
+}
+
+func getRandomComic() (string, string) {
+	content := getHTML(randomLink)
+	return getComicDetails(&content)
+}
+
+func getComicDetails(content *string) (string, string) {
+	name := getComicName(content)
+	link := getImageLink(content)
+	return name, link
 }
 
 func getHTML(link string) string {
