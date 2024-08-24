4ef7e949ffa2deecbc69c1a8089c90900d1dbd21
Ethan Denny <ethandenny@protonmail.com>
2024-08-24 10:47:16 -0230

    It lives

--- main.go
+++ main.go
@@ -1,53 +1,143 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"regexp"
 	"strconv"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 const comicPrefix = "https://xkcd.com/"
 const randomLink = "https://c.xkcd.com/random/comic/"
 
-var comicNameRegex = regexp.MustCompile(`<div id="ctitle">(.*?)</div>`)
-var imageLinkRegex = regexp.MustCompile(`https://imgs.xkcd.com/comics/.*\.(png|jpg)`)
+// TODO: Determine this from the home page
+var ComicsCreated float64 = 2976.0
 
-func main() {
-	name, link := getRandomComic()
-	fmt.Println(name)
-	fmt.Println(link)
-}
+var comicLinkRegex = regexp.MustCompile(`Permanent link to this comic: <a href="(.*?)">`)
 
-func getComicName(content *string) string {
-	matches := comicNameRegex.FindStringSubmatch(*content)
-	if len(matches) > 1 {
-		return matches[1]
-	} else {
-		return ""
+var (
+	BotToken       = flag.String("token", "", "Bot access token")
+	RemoveCommands = true
+)
+
+var s *discordgo.Session
+
+func init() { flag.Parse() }
+
+func init() {
+	var err error
+	s, err = discordgo.New("Bot " + *BotToken)
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
 	}
 }
 
-func getImageLink(content *string) string {
-	return imageLinkRegex.FindString(*content)
+var (
+	integerOptionMinValue = 1.0
+
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name:        "xkcd",
+			Description: "xkcd comics",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionInteger,
+					Name:        "id",
+					Description: "Comic ID",
+					MinValue:    &integerOptionMinValue,
+					MaxValue:    ComicsCreated,
+					Required:    true,
+				},
+			},
+		},
+		{
+			Name:        "xkcd-random",
+			Description: "A random comic",
+		},
+		{
+			Name:        "xkcd-standards",
+			Description: "14 + 1 standards",
+		},
+	}
+
+	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"xkcd": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			options := i.ApplicationCommandData().Options
+			id := options[0].IntValue()
+			link := getComic(id)
+			respondWithComic(s, i, link)
+		},
+		"xkcd-random": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			link := getRandomComic()
+			respondWithComic(s, i, link)
+		},
+		"xkcd-standards": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			link := getComic(927)
+			respondWithComic(s, i, link)
+		},
+	}
+)
+
+func init() {
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
 }
 
-func getComic(number int) (string, string) {
-	content := getHTML(comicPrefix + strconv.Itoa(number))
-	return getComicDetails(&content)
+func main() {
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+	})
+	err := s.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+
+	log.Println("Adding commands...")
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, "", v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		registeredCommands[i] = cmd
+	}
+
+	defer s.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to exit")
+	<-stop
+
+	log.Println("Gracefully shutting down.")
 }
 
-func getRandomComic() (string, string) {
+func respondWithComic(s *discordgo.Session, i *discordgo.InteractionCreate, link string) {
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: link,
+		},
+	})
+}
+
+func getComic(number int64) string {
+	content := getHTML(comicPrefix + strconv.FormatInt(number, 10))
+	return getComicLink(&content)
+}
+
+func getRandomComic() string {
 	content := getHTML(randomLink)
-	return getComicDetails(&content)
-}
-
-func getComicDetails(content *string) (string, string) {
-	name := getComicName(content)
-	link := getImageLink(content)
-	return name, link
+	return getComicLink(&content)
 }
 
 func getHTML(link string) string {
@@ -61,4 +151,13 @@ func getHTML(link string) string {
 		log.Fatal(err)
 	}
 	return string(content)
+}
+
+func getComicLink(content *string) string {
+	matches := comicLinkRegex.FindStringSubmatch(*content)
+	if len(matches) > 1 {
+		return matches[1]
+	} else {
+		return ""
+	}
 }
